ba07a11c880704a064b600ee0de10131025ed185
huanfuan <haunchen@163.com>
2018-10-31 01:26:29 +0800

    1.0.1

---
+++ SuperTextView-demo/app/src/main/java/com/huan/supertextview_demo/ListActivity.java
@@ -0,0 +1,13 @@
+package com.huan.supertextview_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+    }
+}
---
+++ SuperTextView-demo/app/src/main/java/com/huan/supertextview_demo/SuperButtonActivity.java
@@ -0,0 +1,13 @@
+package com.huan.supertextview_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuperButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.super_button_layout);
+    }
+}
